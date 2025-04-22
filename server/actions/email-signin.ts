e8500141f84a1e9d0e724a0414d832ddf942a5de
Natalie Chufistova <nataliech0409@gmail.com>
2025-04-22 08:42:46 +0300

    login errors

--- server/actions/email-signin.ts
+++ server/actions/email-signin.ts
@@ -2,12 +2,28 @@
 
 import { loginSchema } from '@/types/login-schema';
 import {createSafeActionClient} from 'next-safe-action';
+import { db } from '@/server';
+import { eq } from 'drizzle-orm';
+import { users } from '../schema';
 
 const actionClient = createSafeActionClient();
 
 export const emailSignin = actionClient
   .schema(loginSchema)
   .action(async ({ parsedInput: { email, password, code } }) => {
+
+    const existingUser = await db.query.users.findFirst({
+      where: eq(users.email, email),
+    });
+
+    if (existingUser?.email !== email) {
+      return { error: 'Email not found' };
+    };
+
+    // if(!existingUser.emailVerified) {
+    //   return { error: 'Email not verified' };
+    // };
+
     console.log(email, password, code);
-    return {email, password, code};
+    return { success: { email } };
   });
